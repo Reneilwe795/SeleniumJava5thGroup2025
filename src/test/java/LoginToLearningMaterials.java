fc3776d7ed42adc112c565c5cb65a0d6b8bd524b
Reneilwe795 <reneilwesekgwelea795@gmail.com>
2025-10-17 15:21:42 +0200

    Initial commit with project setup and files

---
+++ src/test/java/LoginToLearningMaterials.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginToLearningMaterials {
+
+
+    WebDriver driver;
+
+
+    @BeforeTest
+    public void launchBrowser() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void startBrowser() throws InterruptedException {
+        //driver = new ChromeDriver();
+        driver.get("http://www.ndosiautomation.co.za/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+    }
+    @Test(priority = 1)
+    public void verifyHomePageIsDisplayed(){
+        driver.findElement(By.xpath("//*[@id='overview-hero']/h2")).isDisplayed();
+    }
+    @Test(priority = 2)
+    public void clickLearningMaterials() throws InterruptedException {
+        driver.findElement(By.id("nav-btn-practice")).click();
+        Thread.sleep(2000);
+    }
+    @Test(priority = 3)
+    public void enterEmail() {
+        driver.findElement(By.id("login-email")).sendKeys("rcstest@gmail.com");
+    }
+    @Test(priority = 4)
+    public void enterPassword() {
+        driver.findElement(By.id("login-password")).sendKeys("95354601");
+    }
+    @Test(priority = 5)
+    public void clickLoginButton() throws InterruptedException {
+        driver.findElement(By.id("login-submit")).click();
+        Thread.sleep(2000);
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
