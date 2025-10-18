1034fb35d0c06251fdbcbcba1d0eb4b4896b3056
Reneilwe795 <reneilwesekgwelea795@gmail.com>
2025-10-19 00:30:56 +0200

    created a method to click LearningMaterial

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -11,12 +11,19 @@ public class HomePage {
     @FindBy(id = "overview-hero")
     WebElement homepageTitle_id;
 
+    @FindBy(id = "nav-btn-practice")
+    WebElement LearningMaterial_id;
+
     public HomePage(WebDriver driver) {
         this.driver=driver;
     }
 
     public void verifyHomePageIsDisplayed(){
         homepageTitle_id.isDisplayed();
+    }
+
+    public void clickLearningMaterial() {
+        LearningMaterial_id.click();
     }
 
 }
